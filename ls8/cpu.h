65cedd6d664708addd380ee94c07e8a90b3fa2ea
Matthew Wright <matthewbwright@gmail.com>
2019-04-05 09:06:27 -0700

    adds the instructions for JMP, JEQ, JNE, CMP

--- ls8/cpu.h
+++ ls8/cpu.h
@@ -34,6 +34,7 @@ enum alu_op
 #define POP 0b01000110
 #define PUSH 0b01000101
 #define ST 0b10000100
+#define JMP 01010100
 // TODO: more instructions here. These can be used in cpu_run().
 #define base_address 0x00
 // Function declarations
